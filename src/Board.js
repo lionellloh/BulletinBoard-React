d8dddfe813c2cff122ef582708bb33e8bc7e6771
lionellloh <lionell_loh@mymail.sutd.edu.sg>
2019-01-01 22:25:30 +0800

    add note functionality

--- src/Board.js
+++ src/Board.js
@@ -7,26 +7,28 @@ class Board extends Component {
         super(props)
         this.state = {
             notes: [
-                {
-                    id: 0,
-                    note: "Call Lisa"
-                },
-
-                {
-                    id: 1,
-                    note: "Email John"
-                },
-
-                {
-                    id: 12,
-                    note: "Order printer ink"
-                }
+                // {
+                //     id: 0,
+                //     note: "Call Lisa"
+                // },
+                //
+                // {
+                //     id: 1,
+                //     note: "Email John"
+                // },
+                //
+                // {
+                //     id: 12,
+                //     note: "Order printer ink"
+                // }
             ]
         }
 
         this.eachNote = this.eachNote.bind(this)
         this.update = this.update.bind(this)
         this.remove = this.remove.bind(this)
+        this.add = this.add.bind(this)
+        this.nextId = this.nextId.bind(this)
     }
 
     update(newText, i){
@@ -37,7 +39,23 @@ class Board extends Component {
             )
         }))
     }
+    add(text) {
 
+        this.setState(prevState => (
+            {notes: [...prevState.notes,
+                    {
+                        id: this.nextId(),
+                        note: text
+                    }
+                ]}
+        ))
+    }
+
+    nextId(){
+        this.uniqueId = this.uniqueId || 0
+
+        return this.uniqueId ++
+    }
     remove(id){
         console.log("removing item at ", id)
         this.setState(prevState => (
@@ -63,6 +81,8 @@ class Board extends Component {
         return (
             <div className = "board">
                 {this.state.notes.map(this.eachNote)}
+                <button onClick={this.add.bind(null, "New Note")}
+                id="add"> Add New Note </button>
             </div>
         )
     }
